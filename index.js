7abe120d12d046ac21430df884f1ac222c2592af
Daniel <danbapp@gmail.com>
2022-01-03 16:35:00 -0500

    commit tree c50c216e18ef77cc34d5f9b74218c9f8144bfb9f

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/\W+[^,]/g,'');
+  }
+
+  static titleize(string) {
+    return string.replace(/\b\w/g, l => l.toUpperCase());
+  }
+
 }
